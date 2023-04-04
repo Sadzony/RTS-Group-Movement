b854850f77a37e673fb16a700d7c754cb5280608
Sadzony <szymon.sadunski@gmail.com>
2023-04-04 19:26:09 +0100

    Squad Logger

--- Assets/MyAssets/Scripts/Data/Data Object/Squad.cs
+++ Assets/MyAssets/Scripts/Data/Data Object/Squad.cs
@@ -19,6 +19,11 @@ public class UnitList
         list = new List<Unit>(copy.list);
         set = new HashSet<Unit>(copy.set);
     }
+    public UnitList(HashSet<Unit> p_set)
+    {
+        set = new HashSet<Unit>(p_set);
+        list = new List<Unit>(set);
+    }
     ~UnitList()
     {
         list.Clear();
@@ -61,6 +66,11 @@ public class Squad : UnitList
 {
     private Command command;
 
+    public void SetCommand(Command p_command)
+    {
+        command = p_command;
+    }
+
     public Squad() : base()
     {
         command = new Command();
@@ -68,5 +78,13 @@ public class Squad : UnitList
     public Squad(Squad copy) : base(copy)
     {
         copy.command = command;
+    }
+    public Squad(HashSet<Unit> p_set) : base(p_set)
+    {
+        command = new Command();
+    }
+    public Squad(HashSet<Unit> p_set, Command p_command) : base(p_set)
+    {
+        command = p_command;
     }
 }
--- Assets/MyAssets/Scripts/Data/SquadManager.cs
+++ Assets/MyAssets/Scripts/Data/SquadManager.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class SquadManager : MonoBehaviour
@@ -25,39 +26,41 @@ public class SquadManager : MonoBehaviour
 
     //Member variables
 
-    //Serializable log used for squad selection mechanic
-    [SerializeField] protected List<UnitList> log = new List<UnitList>();
 
-    protected int logSize = 5;
+    //Log used for squad selection mechanic
+    private LinkedList<Squad> log = new LinkedList<Squad>();
+    //Serialized copy of the log
+    [SerializeField] List<Squad> _log = new List<Squad>();
 
-    //List used to output the active squads to screen
-    [SerializeField] protected List<Squad> activeSquads = new List<Squad>();
+    //Debug List used to output the active squads to screen
+    private HashSet<Squad> activeSquads = new HashSet<Squad>();
+    //Serialized copy of the above list
+    [SerializeField] List<Squad> _activeSquads = new List<Squad>();
 
     //Dictionary mapping commands to squads
-    protected Dictionary<Command, Squad> squads = new Dictionary<Command, Squad>();
+    private Dictionary<Command, Squad> squads = new Dictionary<Command, Squad>();
 
     //Functions
     public void AddSquad(Command command, HashSet<Unit> squadMembers)
     {
-        Squad squad = new Squad();
-        squad.SetMembers(squadMembers);
+        Squad squad = new Squad(squadMembers, command);
 
         //Create dictionary entry
         if (!squads.ContainsKey(command))
         {
             squads.Add(command, squad);
-            //activeSquads.Insert(0, squad);
+            activeSquads.Add(squad);
 
-            //Check if the members match a recent log entry
+            //Check if the squads' in the log unit hash set matches the newly added squad's
             bool logMatch = false;
-            foreach (UnitList units in log)
+            foreach (Squad members in log)
             {
-                HashSet<Unit> loggedMembers = units.GetMembers();
+                HashSet<Unit> loggedMembers = members.GetMembers();
                 if (squadMembers.SetEquals(loggedMembers))
                 {
-                    //If it does, bring that set to the top of the list
-                    log.Remove(units);
-                    log.Insert(0, units);
+                    //If it does, bring the new squad to the top of the log and remove the previous log entry
+                    log.Remove(members);
+                    log.AddFirst(squad);
                     logMatch = true;
                     break;
                 }
@@ -65,43 +68,29 @@ public class SquadManager : MonoBehaviour
             //If the new entry did not match the log, then log gets a new entry
             if (!logMatch)
             {
-                //If log isn't full, add list at beginning
-                if (log.Count < logSize)
-                    log.Insert(0, new UnitList(squad));
-                else
-                {
-                    //otherwise make space by removing last entry
-                    log.RemoveAt(log.Count - 1);
-                    log.Insert(0, new UnitList(squad));
-                }
+                log.AddFirst(squad);
             }
         }
+        //Update serialized properties
+        _log = new List<Squad>(log);
+        _activeSquads = new List<Squad>(activeSquads);
     }
-    public void RemoveUnitFromSquad(Command command, Unit unit, bool removeFromLog = true) 
+    public void RemoveUnitFromSquad(Command command, Unit unit) 
     {
         Squad foundSquad;
         if(squads.TryGetValue(command, out foundSquad))
         {
             foundSquad.Remove(unit);
-            if(removeFromLog)
-            {
-                foreach(UnitList units in log)
-                {
-                    //Removes the unit from the log, if it is a member
-                    if (units.Remove(unit))
-                    {
-                        if (units.Count() <= 0)
-                            log.Remove(units);
-                        break;
-                    }
-                }
-            }
             //If the squad has become empty, remove it from dictionary
             if(foundSquad.Count() <= 0)
             {
                 squads.Remove(command);
-                //activeSquads.Remove(foundSquad);
+                activeSquads.Remove(foundSquad);
+                log.Remove(foundSquad);
             }
         }
+        //Update serialized properties
+        _log = new List<Squad>(log);
+        _activeSquads = new List<Squad>(activeSquads);
     }
 }
